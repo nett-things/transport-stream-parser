59f6675f3c7b22f5eb353b40a82a0536fb4b0cb6
Bart Rozworski <112755339+nett-things@users.noreply.github.com>
2026-06-10 11:05:04 +0200

    change name
    changed the name of example.py to demo.py and ts_parser.py to parser.py

---
+++ example/demo.py
@@ -0,0 +1,9 @@
+import ts_parser.parser as tsp
+
+if __name__ == '__main__':
+    TS_packets = tsp.parse_TS('data\\sample.ts', log=True)
+
+    tsp.extract_stream(TS_packets, 257, 'data\\PID257.mp2') # audio
+    tsp.extract_stream(TS_packets, 256, 'data\\PID256.264') # video
+
+    # ffmpeg -i PID256.264 -c copy PID256.mp4
--- example/example.py
+++
@@ -1,9 +0,0 @@
-import ts_parser as tsp
-
-if __name__ == '__main__':
-    TS_packets = tsp.parse_TS('data/sample.ts', log=False)
-
-    tsp.extract_stream(TS_packets, 257, 'data/PID257.mp2') # audio
-    tsp.extract_stream(TS_packets, 256, 'data/PID256.264') # video
-
-    # ffmpeg -i PID256.264 -c copy PID256.mp4
